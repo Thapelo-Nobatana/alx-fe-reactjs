e61888f189ba5b67f0e73b0ebdd1a0379b423941
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-07 14:01:31 +0200

    feat: refactor an existing React application that uses prop drilling, implement the context API

---
+++ alx-react-app-props/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserInfo from './UserInfo';
+
+
+function ProfilePage({ userData }) {
+  return (
+   <UserInfo userData={userData} />
+  )
+}
+
+export default ProfilePage
