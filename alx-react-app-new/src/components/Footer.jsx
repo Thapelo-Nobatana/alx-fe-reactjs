3272bf0859b8f1c9d54fcc146da0b12d9306b139
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-07 13:01:17 +0200

    feat: styling components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer style={{backgroundColor: '#000', textAlign: 'center', padding: '10px', marginTop: '20px'}}>
+            <p style={{ color: 'white'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
