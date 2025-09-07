6334bf807fc53294a81c70b41f836dfbdb591238
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-07 13:24:22 +0200

    feat: import and include the Counter Component in your App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import './App.css'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
      <WelcomeMessage />
      <Header />
      <MainContent />
-     
+     <Counter />
      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
+     <Footer />
     </>
   )
 }
