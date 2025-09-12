b8326cb2fa765503574b6e4f32d09fec01dbcb37
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-12 14:05:19 +0200

    feat: Enhance a user experience by adding functionality for users

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -5,9 +5,9 @@
     const recipe = useRecipeStore(state =>
       state.recipes.find(recipe => recipe.id === recipeId)
     );
-
+   const { favorites, addFavorite, removeFavorite } = useRecipeStore()
     const handleDelete = useRecipeStore((state) => state.deleteRecipe);
-
+    const isFavorite = favorites.includes(recipeId);
     return (
       <div>
         <h1>{recipe.title}</h1>
@@ -15,6 +15,11 @@
         {/* Render EditRecipeForm and DeleteRecipeButton here */}
         <AddRecipeForm />
         <button onClick={handleDelete}>Delete Recipe</button>
+        <button
+         onClick={() => isFavorite ? removeFavorite(recipeId) : addFavorite(recipeId)}
+        >
+            { isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+        </button>
       </div>
     );
   };
