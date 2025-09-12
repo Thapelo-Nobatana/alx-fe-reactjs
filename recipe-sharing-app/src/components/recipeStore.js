2094e8c4e6816667dc7bd98bad319177a083476c
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-12 13:17:06 +0200

    feat: searchTerm filter

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -15,7 +15,7 @@ const useRecipeStore = create((set) => ({
  setRecipes: (recipes) => set({ recipes }),
  deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== id)})),
  updateRecipe: (id, updatedRecipe) => set((state) => ({recipes: state.recipes.map((recipe) => recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe)})),
- setSeerchTerm: (term) => set({searchTerm: term}),
+ setSearchTerm: (term) => set({searchTerm: term}),
  filteredRecipes: [],
  filterRecipes:() => set(state => ({ 
     filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())),
