2e590e4976bf1090330c9738fb8f84bc62c334cc
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-12 14:09:39 +0200

    feat: Enhance the user experience by adding functionality for users

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -11,16 +11,24 @@ const useRecipeStore = create((set) => ({
     },
  ],
  searchTerm: '',
+ filteredRecipes: [],
+ favorites: [],
+ recommendations: [],
  addRecipe: (newRecipe) => set((state) => ({ recipes: [ ...state.recipes, newRecipe ] })),
  setRecipes: (recipes) => set({ recipes }),
  deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== id)})),
  updateRecipe: (id, updatedRecipe) => set((state) => ({recipes: state.recipes.map((recipe) => recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe)})),
  setSearchTerm: (term) => set({searchTerm: term}),
- filteredRecipes: [],
  filterRecipes:() => set(state => ({ 
     filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())),
- }))
-
+ 
+ })),
+addFavorite: (recipeId) => set( state => ({ favorites: [ ...state.favorites, recipeId] })),
+removeFavorite: (recipeId) => set(state => ({ favorites: state.addFavorite.filter( id => id !== recipeId) })),
+generateRecommendations: () => set(state => { 
+    const recommended = state.recipes.filter( recipe => state.favorites.includes(recipe.id) && Math.random() > 0.5 );
+    return { recommendations: recommended }
+})
 }));
 
 
