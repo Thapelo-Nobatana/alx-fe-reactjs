86a80ef12a293ac0de6987ee9939e70309d8fd4c
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-21 10:27:35 +0200

    feat: basic search functionality

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,19 @@
 import axios from "axios";
+import { useState } from "react";
 
 const BASE_URL = "https://api.github.com/users";
 const TOKEN = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const getUser = async (username) => {
+export const fetchUserData = async (username) => {
+  const [loading, setLoading] = useState(false);
   try {
     const response = await axios.get(`${BASE_URL}/${username}`, {
       headers: TOKEN ? { Authorization: `token ${TOKEN}` } : {},
     });
+
     return response.data;
   } catch (error) {
-    console.error("Error fetching user:", error);
+    console.error("Looks like we cant find the user:", error);
     throw error;
   }
 };
