e4efdbe653f30ff6cd0f0d81adfe70a08e17ed08
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-21 10:51:22 +0200

    feat: Search Component and Handle API request

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
 
 const SearchBar = () => {
   const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const handlesubmit = (e) => {
+  const handlesubmit = async (e) => {
     e.preventDefault();
     if (username.trim()) {
       // eslint-disable-next-line no-undef
       onSearch(username);
       setUsername(username);
-      setLoading(true);
+    }
+
+    setLoading(true);
+    setError("");
+
+    try {
+      const data = await fetchUserData(username);
+      setUser(data);
+    } catch (error) {
+      setError("Looks like we can't find the user", error);
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -24,6 +38,8 @@ const SearchBar = () => {
         />
         <button type="submit">{loading ? "Loading..." : "Search"}</button>
       </form>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </>
   );
 };
