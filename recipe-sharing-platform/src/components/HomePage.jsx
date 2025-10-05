10b7a3b26682a495cb91b72976e9d8fbeaa6574f
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-05 15:24:48 +0200

    feat: developed Responsive Layout Desing

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const HomePage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Simulate fetching data
+    setTimeout(() => {
+      fetch("/src/data.json")
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error("Error fetching data:", error));
+    }, 1000);
+  }, []);
+  return (
+    <>
+      <div>Fecthed data</div>
+      <div>
+        {Array.isArray(data) ? (
+          data.map((item, index) => (
+            <div
+              key={index}
+              className="grid grid-col-1 md:grid-col-2 lg:grid-col-4 bg-[#ffffff] rounded-lg item-center hover:shadow-lg p-4 m-4"
+            >
+              <h1>{item.id}</h1>
+              <p>{item.title}</p>
+              <p>{item.summary}</p>
+              <img src={item.image} alt="image" />
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </>
+  );
+};
