5b9e0042586f98e7604814d70f62e797fba10660
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-06 15:02:45 +0200

    feat: modified

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 import "./App.css";
