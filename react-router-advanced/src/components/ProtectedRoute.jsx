8470e698974b4f764987472dc3019f744c7de4ca
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-11 16:32:22 +0200

    chore: routing

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = false; // Replace with actual authentication logic
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  return <div>{children}</div>;
+};
+
+export default ProtectedRoute;
